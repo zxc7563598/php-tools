b12f12808d4941656b809b9d5348b6b7403f3559
苏青安 <992182040@qq.com>
2024-10-17 18:42:47 +0800

    Initial commit

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'vendor/autoload.php';  // 自动加载 Composer 包
-
-use Hejunjie\Tools;
-
-echo json_encode(Tools\IdAddressLookup::getAll(), JSON_UNESCAPED_UNICODE + JSON_UNESCAPED_SLASHES + JSON_PRESERVE_ZERO_FRACTION);
